c7814310f18b104669e9f1ef8a4a759c7f03d1dc
MUHAMMAD ADNAN <160736300+adnansheikha@users.noreply.github.com>
2024-03-14 15:45:36 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+// condition or conditions ? expression1 : expression2
+var userName = "adnan sheikh";
+var pw = 23456;
+// if (username == "adnan") {
+//  console.log("you are successfully login") ;
+// } else {
+//  console.log ("invalid username");
+// }
+userName == "sheikh" || pw == 23456
+    ? console.log("you are logged in")
+    : console.log("invalid username and password");
---
+++ main.ts
@@ -0,0 +1,16 @@
+// condition or conditions ? expression1 : expression2
+
+let userName= "adnan sheikh"
+let pw=23456
+
+// if (username == "adnan") {
+  //  console.log("you are successfully login") ;
+
+// } else {
+
+  //  console.log ("invalid username");
+// }
+
+userName == "sheikh" || pw == 23456
+? console.log("you are logged in")
+: console.log ("invalid username and password");
